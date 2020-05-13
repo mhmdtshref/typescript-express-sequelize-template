cb2ef025a8e28aefd1d61eaa5b2ebbb589da49bc
Mohamed <mshareef@kiitos-tech.com>
2020-05-13 22:57:12 +0300

    Create user sequelize migration models #2

---
+++ migrations/20200513192024-create-user.ts
@@ -0,0 +1,35 @@
+import {
+    QueryInterface,
+    SequelizeStatic
+} from 'sequelize';
+
+export = {
+    up: (queryInterface: QueryInterface, Sequelize: SequelizeStatic) => {
+        return queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+
+            firstName: {
+                type: Sequelize.STRING
+            },
+
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+
+    down: (queryInterface: QueryInterface, Sequelize: SequelizeStatic) => {
+        return queryInterface.dropTable('Users');
+    }
+};
---
+++ models/index.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(__filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
+var db: any   = {};
+
+if (process.env) {
+  var sequelize = new Sequelize(process.env.DATABASE_URL, config);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter((file: string) => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.ts');
+  })
+  .forEach((file: string) => {
+    var model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
