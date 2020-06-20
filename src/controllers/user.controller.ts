9a8969f146670f612d5825bd98b9c570950b76cb
Mohamed <mshareef@kiitos-tech.com>
2020-06-20 16:39:03 +0300

    Create user controller class #8

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,25 +4,27 @@ import { Request, Response, NextFunction } from 'express';
 import { UserService } from '../services';
 import Boom from '@hapi/boom';
 
- export const index = (request: Request, response: Response, errorHandler: NextFunction): void => {
-    // Get request query
-    // const query = request.query;
-
-    // Get request query
-    // const body = request.body;
-
-    // Get request params
-    // const params = request.params;
-
-    UserService.index().then((users) => {
-        // To send response
-        response.status(200).json({
-            success: true,
-            body: {
-                users,
-            },
+export class UserController {
+    index = (request: Request, response: Response, errorHandler: NextFunction): void => {
+        // Get request query
+        // const query = request.query;
+    
+        // Get request query
+        // const body = request.body;
+    
+        // Get request params
+        // const params = request.params;
+    
+        UserService.index().then((users) => {
+            // To send response
+            response.status(200).json({
+                success: true,
+                body: {
+                    users,
+                },
+            });
+        }).catch((error: Error) => {
+            errorHandler(Boom.badRequest(error.message));
         });
-    }).catch((error: Error) => {
-        errorHandler(Boom.badRequest(error.message));
-    });
+    }
 }
